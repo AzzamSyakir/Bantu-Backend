a233ba908160244350bc13649caf151b907ba970
Azzamsyakir <azzamsykir@gmail.com>
2025-02-19 21:28:21 +0700

    feat: add instance producer rabbitmq in service struct

--- src/internal/services/auth_service.go
+++ src/internal/services/auth_service.go
@@ -1,13 +1,18 @@
 package services
 
-import "bantu-backend/src/internal/repository"
+import (
+	"bantu-backend/src/internal/rabbitmq/producer"
+	"bantu-backend/src/internal/repository"
+)
 
 type AuthService struct {
 	UserRepository *repository.UserRepository
+	Producer       *producer.ServicesProducer
 }
 
-func NewAuthService(userRepository *repository.UserRepository) *AuthService {
+func NewAuthService(userRepository *repository.UserRepository, producer *producer.ServicesProducer) *AuthService {
 	AuthService := &AuthService{
+		Producer:       producer,
 		UserRepository: userRepository,
 	}
 	return AuthService
